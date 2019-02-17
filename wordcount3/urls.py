90d164e20f2a99665ff0a3265825b0c0cb4a972d
Monank Rana <ranamonank7383@gmail.com>
2019-02-17 10:13:01 +0530

    changes homepage to home

--- wordcount3/urls.py
+++ wordcount3/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomePage, name="Home"),
+    path('', views.Home, name="Home"),
     path('count/', views.count, name="count"),
     path('Help/', views.Help, name="Help"),
     path('About/', views.About, name="About"),
